a51a4d3fa0d964cb751d266a84d6de7c6226132c
tannerfaust <tannerwellspoken@proton.me>
2026-03-19 22:07:10 +0100

    Initial Commit

---
+++ CueIn/CueInApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueInApp.swift
+//  CueIn
+//
+//  Created by Tanner Fause on 19.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CueInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
